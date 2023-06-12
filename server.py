b3e86ee39e5a1442e9aae0109a6da6c6a764a4de
JGamboaB <jgamboabotero@gmail.com>
2023-06-12 13:45:54 -0600

    Added basic username

--- server.py
+++ server.py
@@ -1,25 +1,37 @@
-#pip install flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+# Variable to store the entered username
+username = ""
+
 @app.route('/')
 def index():
-    return render_template('terminal.html')
+    return render_template('terminal.html', username=username)
 
 @app.route('/execute', methods=['POST'])
 def execute():
     command = request.form['command']
-    result = run_command(command)
-    return result
+    result, updated_username = run_command(command)
+    return jsonify(result=result, username=updated_username)
 
 def run_command(command):
-    if command == 'print':
-        return 'This is an echo.'
-    elif command == 'hello':
-        return 'Hello, world!'
+    global username
+    parts = command.split()
+
+    if parts[0] == 'enter':
+        if len(parts) > 1:
+            last_username = username
+            username = parts[1]
+            return 'Username set to ' + username, last_username
+        else:
+            return 'Please provide a username.', username
+    elif parts[0] == 'print':
+        return 'This is an echo.', username
+    elif parts[0] == 'hello':
+        return 'Hello, world!', username
     else:
-        return 'Command not found.'
+        return 'Command not found.', username
 
 if __name__ == '__main__':
     app.run(debug=True)
